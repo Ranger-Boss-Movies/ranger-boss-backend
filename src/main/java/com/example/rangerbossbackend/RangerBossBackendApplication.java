1d602ecec90f49086c158760563fc27038e718cf
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-22 11:37:48 -0500

    first commit

---
+++ src/main/java/com/example/rangerbossbackend/RangerBossBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rangerbossbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangerBossBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RangerBossBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rangerbossbackend/RangerBossBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rangerbossbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangerBossBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
