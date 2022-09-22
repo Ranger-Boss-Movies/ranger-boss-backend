87d0bffac08bba5a7ae88da4f6bf2f26595fa430
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-22 15:29:12 -0500

    defined genre class

--- src/main/java/data/Genre.java
+++ src/main/java/data/Genre.java
@@ -1,4 +1,31 @@
 package data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.List;
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "genre_name", nullable = false)
+    private String name;
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH},
+            targetEntity = Movie.class)
+    @JoinTable(
+            name="movie_genre",
+            joinColumns = {@JoinColumn(name = "genre_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="movie_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+    @JsonIgnoreProperties("categories")
+    private List<Movie> relatedMovies;
+
 }
