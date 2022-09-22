7b5f6dbbf7eae0e333ad3a0df295033b6aade692
katygil7 <gil.katherine22@gmail.com>
2022-09-22 16:06:05 -0500

    Director class created

--- src/main/java/data/Director.java
+++ src/main/java/data/Director.java
@@ -1,5 +1,27 @@
 package data;
 
-public class Director {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "director_name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "director")
+    @JsonIgnoreProperties("director")
+    private Collection<Movie> movies;
 }
