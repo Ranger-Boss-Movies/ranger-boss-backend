206e69b0b8791649ef7794b33d4ac9c75bf794c4
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-23 14:48:08 -0500

    first commit

---
+++ src/main/resources/static/sql/seeders/genres.sql
@@ -0,0 +1,12 @@
+INSERT INTO `genres` (`genre_name`)
+VALUES
+    ('	Action	'),
+    ('	Adventure	'),
+    ('	Comedy	'),
+    ('	Crime	'),
+    ('	Drama	'),
+    ('	Fantasy	'),
+    ('	Mystery	'),
+    ('	Romance	'),
+    ('	Sci-Fi	'),
+    ('	Sport	');
