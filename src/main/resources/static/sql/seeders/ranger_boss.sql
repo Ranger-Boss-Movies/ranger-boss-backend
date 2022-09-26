0a896b88fb760e71cdcc3e618575994afc21e71a
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-26 09:15:44 -0500

    updated seeder to remove drop tables

--- src/main/resources/static/sql/seeders/ranger_boss.sql
+++ src/main/resources/static/sql/seeders/ranger_boss.sql
@@ -1,16 +1,6 @@
-USE `ranger_boss`;
+USE ranger_boss;
 
 SET NAMES utf8mb4;
-
-DROP TABLE IF EXISTS `actors`;
-CREATE TABLE
-    `actors` (
-        `id` bigint(20) NOT NULL AUTO_INCREMENT,
-        `actor_name` varchar(255) NOT NULL,
-        PRIMARY KEY (`id`),
-        UNIQUE KEY `UK_1a0sjog3wgxkki3882gmp7rrq` (`actor_name`)
-             )
-    ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `actors` (`id`, `actor_name`)
 VALUES
@@ -40,14 +30,6 @@ VALUES
     (23,'Tom Hanks'),
     (24,'Uma Thurman');
 
-DROP TABLE IF EXISTS `directors`;
-CREATE TABLE
-    `directors` (
-        `id` bigint(20) NOT NULL AUTO_INCREMENT,
-        `director_name` varchar(255) NOT NULL,
-        PRIMARY KEY (`id`)
-                )
-    ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `directors` (`id`, `director_name`)
 VALUES
@@ -59,14 +41,6 @@ VALUES
     (6,'John G. Avildsen'),
     (7,'Quentin Tarantino');
 
-DROP TABLE IF EXISTS `genres`;
-CREATE TABLE
-    `genres` (
-        `id` bigint(20) NOT NULL AUTO_INCREMENT,
-        `genre_name` varchar(255) NOT NULL,
-        PRIMARY KEY (`id`)
-             )
-    ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO
     `genres` (`id`, `genre_name`)
@@ -82,22 +56,6 @@ VALUES
     (9,'Sci-Fi'),
     (10,'Sport');
 
-DROP TABLE IF EXISTS `movies`;
-CREATE TABLE
-    `movies` (
-        `id` bigint(20) NOT NULL AUTO_INCREMENT,
-        `plot` varchar(1024) DEFAULT NULL,
-        `poster` varchar(255) DEFAULT NULL,
-        `rating` varchar(255) DEFAULT NULL,
-        `score` double DEFAULT NULL,
-        `title` varchar(255) NOT NULL,
-        `year` bigint(20) DEFAULT NULL,
-        `director_id` bigint(20) DEFAULT NULL,
-        PRIMARY KEY (`id`),
-        KEY `FK5ft3u8k962bmjd8rn2mr77j8d` (`director_id`),
-        CONSTRAINT `FK5ft3u8k962bmjd8rn2mr77j8d` FOREIGN KEY (`director_id`) REFERENCES `directors` (`id`)
-             )
-    ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `movies` (`id`, `plot`, `poster`, `rating`, `score`, `title`, `year`, `director_id`)
 VALUES
@@ -109,19 +67,8 @@ VALUES
     (6,'In a magically realistic version of Toronto, a young man must defeat his new girlfriend\'s seven evil exes one by one in order to win her heart.','https://m.media-amazon.com/images/M/MV5BYWQ2OGIyZTgtZmY5MC00NzY3LTg5NDYtMjdkZjgxZmFhZTMzXkEyXkFqcGdeQXVyOTA3ODI3NDA@._V1_SX300.jpg','PG-13',7.5,'Scott Pilgrim vs. the World',2010,3),
     (7,'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.','https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg','PG-13',8.6,'Interstellar',2014,1),
     (8,'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.','https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg','R',8.9,'Pulp Fiction',1994,7),
-(9,'A small-time Philadelphia boxer gets a supremely rare chance to fight the world heavyweight champion in a bout in which he strives to go the distance for his self-respect.','https://m.media-amazon.com/images/M/MV5BNTBkMjg2MjYtYTZjOS00ODQ0LTg0MDEtM2FiNmJmOGU1NGEwXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg','PG',8.1,'Rocky',1976,6);
+    (9,'A small-time Philadelphia boxer gets a supremely rare chance to fight the world heavyweight champion in a bout in which he strives to go the distance for his self-respect.','https://m.media-amazon.com/images/M/MV5BNTBkMjg2MjYtYTZjOS00ODQ0LTg0MDEtM2FiNmJmOGU1NGEwXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg','PG',8.1,'Rocky',1976,6);
 
-DROP TABLE IF EXISTS `movie_actor`;
-CREATE TABLE
-    `movie_actor` (
-        `movie_id` bigint(20) NOT NULL,
-        `actor_id` bigint(20) NOT NULL,
-        KEY `FKr5wsak3io275nfc8jc5mtup2` (`actor_id`),
-        KEY `FK90bbe6vpr6eoahw20ta95nkta` (`movie_id`),
-        CONSTRAINT `FK90bbe6vpr6eoahw20ta95nkta` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
-        CONSTRAINT `FKr5wsak3io275nfc8jc5mtup2` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`)
-                  )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `movie_actor` (`movie_id`, `actor_id`)
 VALUES
@@ -152,18 +99,6 @@ VALUES
     (9,21),
     (9,22),
     (9,5);
-
-DROP TABLE IF EXISTS `movie_genre`;
-CREATE TABLE
-    `movie_genre` (
-        `movie_id` bigint(20) NOT NULL,
-        `genre_id` bigint(20) NOT NULL,
-        KEY `FK3pdaf1ai9eafeypc7qe401l07` (`genre_id`),
-        KEY `FKg7f38h6umffo51no9ywq91438` (`movie_id`),
-        CONSTRAINT `FK3pdaf1ai9eafeypc7qe401l07` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
-        CONSTRAINT `FKg7f38h6umffo51no9ywq91438` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`)
-)
-    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `movie_genre` (`movie_id`, `genre_id`)
 VALUES
